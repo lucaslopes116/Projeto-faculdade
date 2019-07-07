20756976c53788c0f2086589b6a6c229ceffe21a
Lucas Fernando <lucaslopes116@hotmail.com>
2019-07-07 13:04:04 -0300

    Adicionado componentes de visualização e foto da previsão

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import firebase from 'react-native-firebase'
 
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native'
+import { getLocation, getData } from 'react-native-weather-api';
+import { Card, Button } from 'react-native-elements'
+
 export default class Main extends React.Component {
-  state = { currentUser: null }
+  state = {
+     currentUser: null,
+     city:'',
+     feelsC:'',
+     tempC:'',
+     windKph:'',
+     icon:'',
+     data: []
+    
+    }
 
   componentDidMount() {
     const { currentUser } = firebase.auth()
+
+    getLocation()
+
+    setTimeout(()=>{
+      let data = new getData()
+      console.log(data)
+      this.setState({city: data.city, feelsC: data.feelsC, tempC: data.tempC,windKph:data.windKph,icon:data.icon, data: data })
+
+    },5000)
+  
     this.setState({ currentUser })
+
 }
 
 signOut = () => {
@@ -22,22 +46,65 @@ signOut = () => {
   });
 
 }
-render() {
-    const { currentUser } = this.state
-return (
-      <View style={styles.container}>
-        <Text>
-          Oi {currentUser && currentUser.email}!
-        </Text>
 
-        <TouchableOpacity style={styles.button} onPress={this.signOut}>
-            <Text style={styles.buttonText}>Sair</Text>
-          </TouchableOpacity>
+
+render() {
+    const { currentUser, city, feelsC, tempC, data, icon, windKph  } = this.state
+
+    return (
+      <View style={styles.container}>
+        
+          {data.length === 0 ?
+              <Card style={styles.containerLoadingCard}>
+                <View style={styles.containerLoading}>
+                  <Text>Loading</Text>
+                  <ActivityIndicator size="large" />
+                </View>
+              </Card>
+            : 
+          
+            <Card
+                title={city}
+                image={{uri: icon}}
+                >
+                <Text style={{marginBottom: 10}}>
+                  Sensação térmica de {feelsC}ºC
+                </Text>
+                <Text style={{marginBottom: 10}}>
+                  Temperatura de {tempC}ºC
+                </Text>
+                <Text style={{marginBottom: 10}}>
+                  Velocidade do vento é de  {windKph}km/h
+                </Text>
+                <Button          
+                  backgroundColor='#03A9F4'
+                  onPress={this.signOut}
+                  buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                  title='Sair' />
+            </Card>
+                      
+            }
+          
+            
+          
+              {/* <TouchableOpacity style={styles.button} onPress={this.signOut}>
+                  <Text style={styles.buttonText}>Sair</Text>
+                </TouchableOpacity> */}
       </View>
     )
   }
 }
 const styles = StyleSheet.create({
+  containerLoadingCard:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 2555
+  },
+  containerLoading:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 255
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
